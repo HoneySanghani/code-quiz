3ad5fb0a5a3fbb44e55e78a0e8d2cc2a2d4df7c8
Honey Sanghani <honeysanghani98@gmail.com>
2021-07-18 11:18:10 -0400

    main start page added

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,28 +1,19 @@
-var questionCounter=0;
-var que=document.querySelector(".question");
-var a=document.querySelector("#a");
-var b=document.querySelector("#b");
-var c=document.querySelector("#c");
-var d=document.querySelector("#d");
+var startBtn=document.querySelector(".start-btn");
+var startEl=document.querySelector(".start-quiz");
 var questionsList=function(){
     var questionDataObj={
         id:" ",
         question:"Inside which HTML element do we put the JavaScript?",
-        a:"<script>",
-        b:"<js>",
-        c:"<javascript>",
-        d:"<scripting>",
+        Option:{
+            a:"<script>",
+            b:"<js>",
+            c:"<javascript>",
+            d:"<scripting>",
+        },
         answer:"a"
     }
     displayQuestion(questionDataObj);
 };
-var displayQuestion=function(questionDataObj){
-    questionCounter++;
-    questionDataObj.id=questionCounter;
-    que.textContent=questionDataObj.id+"."+questionDataObj.question;
-    a.textContent="a."+questionDataObj.a;
-    b.textContent="b."+questionDataObj.b;
-    c.textContent="c."+questionDataObj.c;
-    d.textContent="d."+questionDataObj.d;
-};
-questionsList();
+function displayQuestion(){
+    startEl.style.display="none";
+}
